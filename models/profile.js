04eed2b570e9b335167165538d8da70e60548e9f
Ryan <rnowusu@gwu.edu>
2018-08-23 13:48:39 -0400

    added profile model, added get routes, added more folders for file structure/organization

---
+++ models/profile.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+const ProfileSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  date_of_birth: {
+    type: String,
+    required: true
+  },
+  nationality: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  association: {
+    type: String,
+    required: true
+  },
+  team: {
+    type: String,
+    required: true
+  },
+  gender: {
+    type: String,
+    required: true
+  },
+  sports: {
+    type: String,
+    required: true
+  },
+  about: {
+    type: String,
+    required: true
+  },
+  interests: {
+    type: String,
+    required: true
+  },
+  charities: {
+    type: String,
+    required: true
+  },
+  social_media_handles: {
+    type: Object,
+    required: true
+  },
+  pets: {
+    type: Boolean,
+    required: true
+  },
+  drinks_alcohol: {
+    type: Boolean,
+    required: true
+  },
+  married: {
+    type: Boolean,
+    required: true
+  },
+  profileImage: {
+    type: String,
+    required: false
+  }
+});
+
+const Profile = module.exports = mongoose.model('Profile', ProfileSchema)
--- routes/route.js
+++ routes/route.js
@@ -5,5 +5,30 @@ const multer = require('multer');
 
 
 
+  const Profile = require('../models/profile.js');
+
+  router.get('/profiles', function(req, res, next){
+    console.log("Params are ");
+    console.log(req.params);
+    Profile.find(function(err, profiles){
+      res.json(profiles);
+    })
+  });
+
+  router.get('/profiles/:id', function(req, res, next){
+    // res.send('Enter your profile info here');
+    Profile.findById(req.params.id, function(err, result){
+      console.log("Params are ");
+      console.log(req.params);
+      if(err){
+        res.json(err)
+      } else{
+        console.log(result);
+        res.json(result);
+        // res.render('result', {first_name: result.first_name, last_name: result.last_name, phone: result.phone});
+      }
+    })
+  });
+
 
 module.exports = router;
